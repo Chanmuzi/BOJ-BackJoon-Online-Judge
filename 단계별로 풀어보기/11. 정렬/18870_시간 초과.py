bbc7b5aa80397d6739879c1e315702fae1a2dad4
Chanmuzi <101971295+Chanmuzi@users.noreply.github.com>
2022-07-13 22:56:00 +0900

    좌표 압축

---
+++ 단계별로 풀어보기/11. 정렬/18870_시간 초과.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+# 입력받는 숫자들을 정수로 변환하여 리스트에 추가
+num_list = list(map(int,sys.stdin.readline().split()))
+# 숫자리스트의 중복을 제거하고 리스트로 재변환 후 오름차순 정렬
+sorted_list = sorted(list(set(num_list)))
+
+# 정렬된 리스트의 인덱스를 찾아 출력
+for i in range(n):
+    print(sorted_list.index(num_list[i]), end=' ')
